f45ca9dfd83356ab5fe7fa1379500d290ea26c9e
akshara <aksharakabra18@gmail.com>
2026-08-31 21:39:48 +0530

    Create InsertionSort.cpp

---
+++ Lab02-Sorting-Searching/InsertionSort.cpp
@@ -0,0 +1,33 @@
+// Insertion Sort program
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a[100];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    // Insertion Sort
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
