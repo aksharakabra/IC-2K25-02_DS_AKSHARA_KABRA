b5db0292417b9a2fc024796a0f585bec48cd87e5
akshara <aksharakabra18@gmail.com>
2026-09-04 21:46:20 +0530

    Implement basic stack functionality in C++
    Added a stack implementation with push, pop, peek, and display functions.

--- stack-type/Normal-Stack.cpp
+++ stack-type/Normal-Stack.cpp
@@ -1,1 +1,79 @@
 // stack
+#include <iostream>
+using namespace std;
+class myStack{
+    private:
+    int *arr;
+    int capacity, top;
+    public:
+    // its constructor
+    myStack(int cap){
+        capacity = cap;
+        arr= new int[capacity];
+        top= -1;
+    }
+    // empty and full functions
+    bool isempty(){
+        return top == -1;
+        cout<<"stack is empty";
+    }
+    bool isfull(){
+        return top == capacity - 1;
+         cout<<"stack is full\n";
+    }
+    // push then pop
+    void push(int x){
+       if (isfull()){ 
+           cout<<"overflow stack\n";
+           return;
+        }
+       arr[++top]= x;
+       cout<<"we pushed a number\n\n";
+    }
+
+    int pop(){
+        if (isempty()){
+            cout<<"underflow stack\n";
+            return -1;
+        }
+        cout<<"we pulled a number ";
+        return arr[top--];
+    }
+
+    // peek
+    int peek(){
+        if (isempty()){
+            cout<<"underflow stack\n";
+            return -1;
+        }  
+        cout<<"the top is ";
+        return arr[top];
+    }
+    // display
+void display(){
+    if (isempty()){
+            cout<<"underflow stack\n";
+            return;
+        }
+else
+        {cout<<"\nthe stack is: ";
+    for(int i=0; i<=top; i++){
+        cout<<arr[i]<<endl;}}}
+};
+
+    int main(){
+        myStack s(4);
+        s.push(23);
+        s.push(25);
+        s.push(24);
+        cout<<s.pop()<<endl;
+        s.push(26);
+        s.push(66);
+        cout<<s.peek()<<endl;
+        cout<<(s.isempty()? "yes": "no")<<endl;
+        cout<<(s.isfull()? "yes": "no");
+        s.display();
+        return 0;
+        return 0;
+
+}
